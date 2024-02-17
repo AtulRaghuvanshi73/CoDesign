97e7f6de3bc7d8d7dca6eb54ae9fe8043696a822
AtulRaghuvanshi73 <atulraghuvanshi09@gmail.com>
2024-02-18 00:55:08 +0530

    feat: added emoji reactions

--- components/Live.tsx
+++ components/Live.tsx
@@ -93,6 +93,10 @@ const Live = () => {
         }
     },[updateMyPresence]);
 
+    const setReactions = useCallback((reaction: string) => {
+        setCursorState({mode: CursorMode.Reaction, reaction, isPressed: false})
+    }, [])
+
     return (
         <div
             onPointerMove={handlePointerMove}
@@ -117,9 +121,7 @@ const Live = () => {
 
             {cursorState.mode === CursorMode.ReactionSelector && (
                 <ReactionSelector 
-                    setReaction={(reaction) => {
-                        setCursorState({mode: CursorMode.Reaction, reaction, isPressed: false})
-                    }}
+                    setReaction={setReactions}
                 />
             )}
 
